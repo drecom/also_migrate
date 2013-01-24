d35e65f7aafa108f18614cec26ed102c90dd0638
gussan <egussan@gmail.com>
2013-01-24 16:54:38 +0900

    Write migration logs with say_with_time

--- lib/also_migrate/migrator.rb
+++ lib/also_migrate/migrator.rb
@@ -1,6 +1,6 @@
 module AlsoMigrate
   module Migrator
-    
+
     def self.included(base)
       unless base.included_modules.include?(InstanceMethods)
         base.send :include, InstanceMethods
@@ -10,9 +10,9 @@ module AlsoMigrate
         end
       end
     end
-    
+
     module InstanceMethods
-      
+
       def migrate_with_also_migrate
         (::AlsoMigrate.configuration || []).each do |config|
           AlsoMigrate.create_tables(config)
@@ -23,14 +23,31 @@ module AlsoMigrate
       ensure
         migrate_without_also_migrate
       end
-      
+
       module AlsoMigrate
-        class <<self
-        
+        class << self
+
+          def write(text="")
+            puts(text)
+          end
+
+          def say(message, subitem=false)
+            write "#{subitem ? "   ->" : "--"} #{message}"
+          end
+
+          def say_with_time(message)
+            say(message)
+            result = nil
+            time = Benchmark.measure { result = yield }
+            say "%.4fs" % time.real, :subitem
+            say("#{result} rows", :subitem) if result.is_a?(Integer)
+            result
+          end
+
           def connection
             ActiveRecord::Base.connection
           end
-        
+
           def create_tables(config)
             [ config[:destination] ].flatten.compact.each do |new_table|
               if !connection.table_exists?(new_table) && connection.table_exists?(config[:source])
@@ -45,29 +62,37 @@ module AlsoMigrate
                         WHERE Name = '#{config[:source]}'
                       SQL
                     end
-                  connection.execute(<<-SQL)
-                    CREATE TABLE #{new_table} #{engine}
-                    AS SELECT #{columns.join(',')}
-                    FROM #{config[:source]}
-                    WHERE false;
-                  SQL
+                  say_with_time "create_table(#{new_table.inspect})" do
+                    connection.execute(<<-SQL)
+                      CREATE TABLE #{new_table} #{engine}
+                      AS SELECT #{columns.join(',')}
+                      FROM #{config[:source]}
+                      WHERE false;
+                    SQL
+                  end
                   [ config[:indexes] ].flatten.compact.each do |column|
-                    connection.add_index(new_table, column)
+                    say_with_time "add_index(#{new_table.inspect}, #{column.inspect})" do
+                      connection.add_index(new_table, column)
+                    end
                   end
                 else
                   if connection.class.to_s.include?('SQLite')
                     col_string = connection.columns(config[:source]).collect {|c|
                       "#{c.name} #{c.sql_type}"
                     }.join(', ')
-                    connection.execute(<<-SQL)
-                      CREATE TABLE #{new_table}
-                      (#{col_string})
-                    SQL
+                    say_with_time "create_table(#{new_table.inspect})" do
+                      connection.execute(<<-SQL)
+                        CREATE TABLE #{new_table}
+                        (#{col_string})
+                      SQL
+                    end
                   else
-                    connection.execute(<<-SQL)
-                      CREATE TABLE #{new_table}
-                      LIKE #{config[:source]};
-                    SQL
+                    say_with_time "create_table(#{new_table.inspect})" do
+                      connection.execute(<<-SQL)
+                        CREATE TABLE #{new_table}
+                        LIKE #{config[:source]};
+                      SQL
+                    end
                   end
                 end
               end
@@ -78,14 +103,18 @@ module AlsoMigrate
                 if config[:add]
                   config[:add].each do |column|
                     unless columns.include?(column[0])
-                      connection.add_column(*([ new_table ] + column))
+                      say_with_time "add_column(#{new_table.inspect}, #{column.inspect})" do
+                        connection.add_column(*([ new_table ] + column))
+                      end
                     end
                   end
                 end
                 if config[:subtract]
                   [ config[:subtract] ].flatten.compact.each do |column|
                     if columns.include?(column)
-                      connection.remove_column(new_table, column)
+                      say_with_time "add_column(#{new_table.inspect}, #{column.inspect})" do
+                        connection.remove_column(new_table, column)
+                      end
                     end
                   end
                 end
